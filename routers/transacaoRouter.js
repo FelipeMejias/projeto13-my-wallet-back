d8be3374f8db32fd6699dc614942253d0dee392c
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-05-18 16:32:22 -0300

    Preparando revisão de código

--- routers/transacaoRouter.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import { getTransacoes, postTransacoes } from '../controllers/transacaoController.js'
-const transacaoRouter=Router()
-
-transacaoRouter.post('/transacoes',postTransacoes)
-transacaoRouter.get('/transacoes',getTransacoes)
-
-export default transacaoRouter
